02ee4bbf5ab0e596de96286cffbfb0086a399e5f
Arpita Jain <arpitajain0628@gmail.com>
2024-03-31 23:54:41 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const userSchema=new mongoose.SchemaTypes({
+const userSchema=new mongoose.Schema({
     username:{
         type: String,
         required:true,
