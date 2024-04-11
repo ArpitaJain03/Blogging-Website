ac7a25d9bec94576533c3c4ac9bfea0246b04c7c
Arpita Jain <arpitajain0628@gmail.com>
2024-04-11 19:18:10 +0530

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -7,14 +7,14 @@ export const test= (req,res)=>{
 };
 
 export const updateUser= async(req,res,next)=>{
-    if(req.user._id != req.params.userId){
+    if(req.user.id !== req.params.userId){
         return next(errorHandler(403,'You are not allowed to update this user'));
     }
     if(req.body.password){
         if(req.body.password.length<6){
             return next(errorHandler(400,'Password must be atleast 6 characters'));
         }
-        req.boay.password=bcryptjs.hashSync(req.body.password,10);
+        req.body.password=bcryptjs.hashSync(req.body.password,10);
     }
     if(req.body.username){
         if(req.body.username.length < 7 || req.body.username.length>20){
@@ -29,6 +29,7 @@ export const updateUser= async(req,res,next)=>{
         if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
             return next(errorHandler(400,'Username can only contain letters and numbers'));
         }
+    }
         try{
             const updatedUser=await User.findByIdAndUpdate(req.params.userId,{
                 $set:{
@@ -38,10 +39,9 @@ export const updateUser= async(req,res,next)=>{
                     password:req.body.password,
                 },
             },{new:true});
-            const {password,...rest}=updateUser._doc;
+            const {password,...rest}=updatedUser._doc;
             res.status(200).json(rest);
         } catch(error){
             next(error);
         }
-    }
-}
+    };
